14e83bfaf8c844acd39457588790e0e9526b6b98
João Valente <filipevalente@gmail.com>
2013-10-19 18:27:37 +0100

    enhances validation on battle and gem creation

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -2,6 +2,9 @@ class Battle < ActiveRecord::Base
   belongs_to :rubygem_x, foreign_key: "rubygem_x_id", class_name: "Rubygem"
   belongs_to :rubygem_y, foreign_key: "rubygem_y_id", class_name: "Rubygem"
 
-  accepts_nested_attributes_for :rubygem_x
-  accepts_nested_attributes_for :rubygem_y
+  accepts_nested_attributes_for :rubygem_x, reject_if: proc { |attributes| attributes['name'].blank? }
+  accepts_nested_attributes_for :rubygem_y, reject_if: proc { |attributes| attributes['name'].blank? }
+
+  validates_presence_of :rubygem_x
+  validates_presence_of :rubygem_y
 end
