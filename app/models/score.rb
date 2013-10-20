7a153361e345066fd7091a79c858ff7dadb41a58
Tomé Duarte <tome.duarte@gmail.com>
2013-10-20 17:30:49 +0100

    Added scoring to Score model.

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,24 @@
 class Score < ActiveRecord::Base
+  belongs_to :rubygem
+
+  def expired?
+    DateTime.now > expires_at
+  end
+
+  def score
+    if expired?
+      # get a metric instance and its score for this gem
+      metric = kind.constantize.new rubygem.metadata
+      score = metric.score
+
+      # update status
+      update_attribute :result, score
+      update_attribute :expires_at, DateTime.now + metric.expires_at
+
+      # return the score
+      metric.score
+    else
+      result
+    end
+  end
 end
