a4bf92376de65fc7c8d2b78128a7955b9c4884c1
Gonçalo Silva <goncalossilva@gmail.com>
2013-10-20 12:26:22 +0100

    Account for repositories that don't have a README

--- metrics/must-doc_quality.rb
+++ metrics/must-doc_quality.rb
@@ -17,53 +17,52 @@ require 'github/markup'
 # Acts As Paranoid: 0.43928847641144625
 # Spree: 0.09749739311783108
 # Rails: 0.006367881618048396
-class DocumentationCodeRatioCounter
-  include ActionView::Helpers::SanitizeHelper
 
-  def run
-    print "user/repo: "
-    repo_url = gets.chomp
+include ActionView::Helpers::SanitizeHelper
 
+print "user/repo: "
+repo_url = gets.chomp
+
+Dir.mktmpdir do |tmpdir|
+  begin
+    # Get README and write in temp dir
     readme = Octokit.readme(repo_url)
-    Dir.mktmpdir do |tmpdir|
-      # Write REAME in temp dir
-      File.open(File.join(tmpdir, readme.name), 'w') do |file|
-        file.write(
-          Base64.decode64(readme.content).encode(
-            'UTF-8',
-            {:invalid => :replace, :undef => :replace, :replace => '?'}
-          )
+    File.open(File.join(tmpdir, readme.name), 'w') do |file|
+      file.write(
+        Base64.decode64(readme.content).encode(
+          'UTF-8',
+          {:invalid => :replace, :undef => :replace, :replace => '?'}
         )
-      end
-
-      # Clone wiki in temp dir
-      repo =  Octokit.repository('rails/rails')
-      if repo.has_wiki
-        `git clone --depth 1 git@github.com:#{repo_url}.wiki.git #{tmpdir}/wiki`
-      end
-
-      # Get all markdown/textile documentation
-      html = ""
-      Dir.glob("#{tmpdir}/**/*.*").each do |path|
-        html << GitHub::Markup.render(path).html_safe
-      end
-      html.gsub!(/\n/, '')
-
-      # Count code characters
-      code_char_count = 0
-      html.scan(/<code.*?>(.*?)<\/code>/m).flatten.map do |code|
-        code_char_count += strip_tags(code).length
-      end
-
-      # Count total characters
-      total_char_count = strip_tags(html).length
-
-      # Print info
-      puts "Total characters: #{total_char_count}"
-      puts "Code characters: #{code_char_count}"
-      puts "Code/total ratio: #{code_char_count / total_char_count.to_f}"
+      )
     end
+  rescue Octokit::NotFound
+    # No README, that's OK. Actually, that's not OK, but no reason to blow up.
   end
-end
 
-DocumentationCodeRatioCounter.new.run
+  # Clone wiki in temp dir
+  repo =  Octokit.repository('rails/rails')
+  if repo.has_wiki
+    `git clone --depth 1 git@github.com:#{repo_url}.wiki.git #{tmpdir}/wiki`
+  end
+
+  # Get all markdown/textile documentation
+  html = ""
+  Dir.glob("#{tmpdir}/**/*.*").each do |path|
+    html << GitHub::Markup.render(path).html_safe
+  end
+  html.gsub!(/\n/, '')
+
+  # Count code characters
+  code_char_count = 0
+  html.scan(/<code.*?>(.*?)<\/code>/m).flatten.map do |code|
+    code_char_count += strip_tags(code).length
+  end
+
+  # Count total characters
+  total_char_count = strip_tags(html).length
+
+  # Print info
+  puts "Total characters: #{total_char_count}"
+  puts "Code characters: #{code_char_count}"
+  puts "Code/total ratio: #{code_char_count / total_char_count.to_f}"
+end
