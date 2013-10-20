2d2b2c83b767d7666a7ce056b162dd75121c9a29
Tomé Duarte <tome.duarte@gmail.com>
2013-10-20 19:47:29 +0100

    Good available_metric_results for Rubygem.

--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -28,13 +28,31 @@ class Rubygem < ActiveRecord::Base
   end
 
   private
-  def available_metrics
-    # TODO
-    # read the list of available metrics
+
+  def available_metric_results
+    # read the list of available metrics from configuration file
+    configuration_file = File.join(Rails.root.to_s, 'config', 'metrics.yml')
+    metric_classnames = YAML.load_file(configuration_file)['metrics_classnames']
 
     # create or drop objects appropriately
+    current_names = metric_results.collect &:kind
+
+    transaction do
+      ## do we have any deprecated metric?
+      deprecated_metric_results = current_names - metric_classnames
+      deprecated_metric_results.each do |name|
+        metric_results.where(kind: name).destroy_all
+      end
+
+      ## are we missing any metric?
+      new_metric_results = metric_classnames - current_names
+      new_metric_results.each do |name|
+        metric_results.create kind: name
+      end
+    end
 
     # return the list of available metrics
+    metric_results
   end
 
   def update_score
