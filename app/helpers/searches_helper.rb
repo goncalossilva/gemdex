d1e2f53b9f619631ee3ad8e9a2a24b506d5fa6b7
João Valente <filipevalente@gmail.com>
2013-10-20 20:16:22 +0100

    shows and creates ruby gems. search list may link to rubygem#show or create.

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,10 @@
 module SearchesHelper
+  def path_to_rubygem_or_item(name)
+    result = Rubygem.find_by_name(name)
+    if result.nil?
+      button_to name, {action: "create", controller: "rubygems", rubygem: { name: name } }
+    else
+      link_to name, rubygem_path(result)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Gemdex::Application.routes.draw do
   resources :battles, only: [:show, :create]
   resources :searches, only: [:show, :create]
 
-  # resources :rubygems
+  resources :rubygems, only: [:show, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
